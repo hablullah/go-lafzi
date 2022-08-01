2e49dd0c9d294cf6ee0358e617db53ac8dc0c3a2
Radhi Fadlillah <m.radhi.f@gmail.com>
2022-08-01 18:48:15 +0700

    Refactor Arabic and phonetic code

---
+++ arabic.go
@@ -0,0 +1,126 @@
+package lafzi
+
+import (
+	"strings"
+
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
+
+var arabicUnicodeNormalizer = transform.Chain(norm.NFKD, norm.NFKC)
+
+func ArabicToPhonetic(s string) string {
+	normal, _, err := transform.String(arabicUnicodeNormalizer, s)
+	if err == nil {
+		s = normal
+	}
+
+	var sb strings.Builder
+	for _, r := range s {
+		replacementRunes := transformArabicRune(r)
+		for _, rr := range replacementRunes {
+			sb.WriteRune(rr)
+		}
+	}
+
+	return sb.String()
+}
+
+func transformArabicRune(r rune) []rune {
+	switch r {
+	case jeem, thal, zain, zah:
+		return []rune{'z'}
+	case hah, khah, heh:
+		return []rune{'h'}
+	case hamza, alef, ain,
+		alefWithHamzaAbove, alefWithHamzaBelow,
+		yehWithHamzaAbove, wawWithHamzaAbove:
+		return []rune{'x'}
+	case theh, seen, sheen, sad:
+		return []rune{'s'}
+	case dal, dad:
+		return []rune{'d'}
+	case tehMarbuta, teh, tah:
+		return []rune{'t'}
+	case qaf, kaf:
+		return []rune{'k'}
+	case ghain:
+		return []rune{'g'}
+	case feh:
+		return []rune{'f'}
+	case meem:
+		return []rune{'m'}
+	case noon:
+		return []rune{'n'}
+	case lam:
+		return []rune{'l'}
+	case beh:
+		return []rune{'b'}
+	case yeh:
+		return []rune{'y'}
+	case waw:
+		return []rune{'w'}
+	case reh:
+		return []rune{'r'}
+	case fathatan:
+		return []rune{'a', 'n'}
+	case dammatan:
+		return []rune{'u', 'n'}
+	case kasratan:
+		return []rune{'i', 'n'}
+	case fatha:
+		return []rune{'a'}
+	case damma:
+		return []rune{'u'}
+	case kasra:
+		return []rune{'i'}
+	case sukun:
+		return []rune{'0'}
+	default:
+		return nil
+	}
+}
+
+const (
+	hamza              = '\u0621'
+	alefWithHamzaAbove = '\u0623'
+	wawWithHamzaAbove  = '\u0624'
+	alefWithHamzaBelow = '\u0625'
+	yehWithHamzaAbove  = '\u0626'
+	alef               = '\u0627'
+	beh                = '\u0628'
+	tehMarbuta         = '\u0629'
+	teh                = '\u062A'
+	theh               = '\u062B'
+	jeem               = '\u062C'
+	hah                = '\u062D'
+	khah               = '\u062E'
+	dal                = '\u062F'
+	thal               = '\u0630'
+	reh                = '\u0631'
+	zain               = '\u0632'
+	seen               = '\u0633'
+	sheen              = '\u0634'
+	sad                = '\u0635'
+	dad                = '\u0636'
+	tah                = '\u0637'
+	zah                = '\u0638'
+	ain                = '\u0639'
+	ghain              = '\u063A'
+	feh                = '\u0641'
+	qaf                = '\u0642'
+	kaf                = '\u0643'
+	lam                = '\u0644'
+	meem               = '\u0645'
+	noon               = '\u0646'
+	heh                = '\u0647'
+	waw                = '\u0648'
+	yeh                = '\u064A'
+	fathatan           = '\u064B'
+	dammatan           = '\u064C'
+	kasratan           = '\u064D'
+	fatha              = '\u064E'
+	damma              = '\u064F'
+	kasra              = '\u0650'
+	sukun              = '\u0652'
+)
---
+++ phonetic.go
@@ -0,0 +1,161 @@
+package lafzi
+
+import (
+	"regexp"
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
+
+var (
+	rxVowelPrefix   = regexp.MustCompile(`(?i)^[aiu]`)
+	rxHamzahI       = regexp.MustCompile(`(?i)i([au])`)
+	rxHamzahU       = regexp.MustCompile(`(?i)u([ai])`)
+	rxHamzahPrefix  = regexp.MustCompile(`(?i)^([^aiu0])?([^aiu0])0?([^aiu0])([aiu])`)
+	rxMaddaA        = regexp.MustCompile(`(?i)ax([^aiu]|$)`)
+	rxMaddaI        = regexp.MustCompile(`(?i)iy([^aiu]|$)`)
+	rxMaddaU        = regexp.MustCompile(`(?i)uw([^aiu]|$)`)
+	rxTajweedIkhfa  = regexp.MustCompile(`(?i)n0?g`)
+	rxTajweedIqlab  = regexp.MustCompile(`(?i)n0?b`)
+	rxTajweedIdgham = regexp.MustCompile(`(?i)n0?([ynmwlr])`)
+	rxAlifLamSyams  = regexp.MustCompile(`(?i)x([aiu]?)l([zsdtnlr])`)
+	rxUnusedX       = regexp.MustCompile(`(?i)x([^aiu0])`)
+	rxHangingVowel  = regexp.MustCompile(`(?i)[aiu]$`)
+
+	phoneticUnicodeNormalizer = transform.Chain(
+		norm.NFKD,
+		runes.Remove(runes.In(unicode.Mn)),
+		norm.NFKC,
+	)
+
+	similarSoundingRunesCleaner = runes.Map(func(r rune) rune {
+		switch r {
+		case 'o':
+			return 'a'
+		case 'e':
+			return 'i'
+		case 'v', 'p':
+			return 'f'
+		case 'q':
+			return 'k'
+		case 'j':
+			return 'z'
+		case '\'', '`', '‘':
+			return 'x'
+		default:
+			return r
+		}
+	})
+
+	invalidPhoneticRunesCleaner = runes.Remove(
+		runes.Predicate(func(r rune) bool {
+			switch r {
+			case 'z', 'h', 'x', 's', 'd', 't', 'k', 'g',
+				'f', 'm', 'n', 'l', 'b', 'y', 'w', 'r',
+				'a', 'u', 'i', '0':
+				return false
+			default:
+				return true
+			}
+		}),
+	)
+)
+
+func NormalizePhonetic(s string) string {
+	// Normalize unicode
+	normal, _, err := transform.String(phoneticUnicodeNormalizer, s)
+	if err == nil {
+		s = normal
+	}
+
+	// Convert string to lowercase
+	s = strings.ToLower(s)
+
+	// Normalize similar sounding runes, e.g. 'p' => 'f', 'e' => 'i'
+	s = similarSoundingRunesCleaner.String(s)
+
+	// Mark possible hamzah location
+	s = rxHamzahI.ReplaceAllString(s, "ix$1")
+	s = rxHamzahU.ReplaceAllString(s, "ux$1")
+
+	// Normalize space and its vowels, e.g. 'amman antum' => 'xammanxantum'
+	s = normalizeSpaces(s)
+
+	// Remove invalid (or disallowed) phonetic runes
+	s = invalidPhoneticRunesCleaner.String(s)
+
+	// Merge identic adjacent runes, e.g. 'amman' => 'aman'
+	s = mergeIdenticAdjacentRunes(s)
+
+	// Normalize alif or hamzah 'x' in prefix
+	s = rxHamzahPrefix.ReplaceAllString(s, "${1}${4}${2}${3}${4}")
+
+	// Remove invisible 'al' or in tahweed called 'alif lam syamsiah'
+	s = rxAlifLamSyams.ReplaceAllString(s, "${1}${2}")
+
+	// Remove unused alif or hamzah 'x', e.g. 'rabixlxalamin' => 'rabilalamin'
+	s = rxUnusedX.ReplaceAllString(s, "${1}")
+	s = strings.TrimPrefix(s, "x")
+
+	// Remove madda i.e. vowel that spelled a bit long
+	s = rxMaddaA.ReplaceAllString(s, "a${1}")
+	s = rxMaddaI.ReplaceAllString(s, "i${1}")
+	s = rxMaddaU.ReplaceAllString(s, "u${1}")
+
+	// Apply tajweed rules, i.e. ikhfa, iqlab, and idgham
+	s = rxTajweedIkhfa.ReplaceAllString(s, "n0")
+	s = rxTajweedIqlab.ReplaceAllString(s, "m0b")
+	s = rxTajweedIdgham.ReplaceAllString(s, "$1")
+
+	// Remove sukun (stop mark)
+	s = strings.ReplaceAll(s, "0", "")
+
+	// Remove hanging vowel, e.g. 'amanu' => 'aman'
+	s = rxHangingVowel.ReplaceAllString(s, "")
+
+	// Normalize diphtong
+	s = strings.ReplaceAll(s, "ai", "ay")
+	s = strings.ReplaceAll(s, "au", "aw")
+	s = strings.ReplaceAll(s, "sh", "s")
+	s = strings.ReplaceAll(s, "ts", "s")
+	s = strings.ReplaceAll(s, "sy", "s")
+	s = strings.ReplaceAll(s, "kh", "h")
+	s = strings.ReplaceAll(s, "ch", "h")
+	s = strings.ReplaceAll(s, "zh", "z")
+	s = strings.ReplaceAll(s, "dz", "z")
+	s = strings.ReplaceAll(s, "dh", "d")
+	s = strings.ReplaceAll(s, "th", "t")
+	s = strings.ReplaceAll(s, "gh", "g")
+
+	return s
+}
+
+func normalizeSpaces(s string) string {
+	words := strings.Fields(s)
+	for i, word := range words {
+		// If word started with 'a', 'i', 'u', add 'x' in front of it.
+		if rxVowelPrefix.MatchString(word) {
+			words[i] = "x" + word
+			continue
+		}
+	}
+	return strings.Join(words, "")
+}
+
+func mergeIdenticAdjacentRunes(s string) string {
+	var sb strings.Builder
+
+	src := []rune(s)
+	for i := 0; i < len(src)-1; i++ {
+		if src[i] == src[i+1] {
+			continue
+		}
+		sb.WriteRune(src[i])
+	}
+
+	sb.WriteRune(src[len(src)-1])
+	return sb.String()
+}
