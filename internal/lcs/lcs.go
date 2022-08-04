6ade641a94c0a021142652a1f8bca0944e8ae724
Radhi Fadlillah <m.radhi.f@gmail.com>
2022-08-04 13:06:39 +0700

    Return LCS score as is

--- internal/lcs/lcs.go
+++ internal/lcs/lcs.go
@@ -1,7 +1,5 @@
 package lcs
 
-import "math"
-
 // Score calculate the scores for query inside the document
 // using LCS length and its compactness.
 func Score(docTokens, queryTokens []string) float64 {
@@ -9,26 +7,9 @@ func Score(docTokens, queryTokens []string) float64 {
 	edits := myersDiff(docTokens, queryTokens, 0, 0)
 
 	// Fetch LCS from the edits
-	lcs, lcsIndexes := getLCS(docTokens, queryTokens, edits)
+	lcs, _ := getLCS(docTokens, queryTokens, edits)
 
 	// Calculate LCS score
 	lcsScore := float64(len(lcs)) / float64(len(queryTokens))
-
-	// Calculate LCS compactness
-	compactScore := calcCompactScore(lcsIndexes)
-
-	// Return the final score
-	return lcsScore * compactScore
-}
-
-func calcCompactScore(indexes []int) float64 {
-	var sum float64
-	for i := 0; i < len(indexes)-1; i++ {
-		nextIdx := float64(indexes[i+1])
-		currentIdx := float64(indexes[i])
-		idxDiff := math.Abs(nextIdx - currentIdx)
-		sum += 1.0 / idxDiff
-	}
-
-	return sum / float64(len(indexes)-1)
+	return lcsScore
 }
