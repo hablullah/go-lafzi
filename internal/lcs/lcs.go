66fc7655cc3eaead758464e48d46b441d3ef2890
Radhi Fadlillah <m.radhi.f@gmail.com>
2022-08-04 21:28:30 +0700

    Return the compactness score

--- internal/lcs/lcs.go
+++ internal/lcs/lcs.go
@@ -1,5 +1,7 @@
 package lcs
 
+import "math"
+
 // Score calculate the scores for query inside the document
 // using LCS length and its compactness.
 func Score(docTokens, queryTokens []string) float64 {
@@ -7,9 +9,30 @@ func Score(docTokens, queryTokens []string) float64 {
 	edits := myersDiff(docTokens, queryTokens, 0, 0)
 
 	// Fetch LCS from the edits
-	lcs, _ := getLCS(docTokens, queryTokens, edits)
+	lcs, lcsIndexes := getLCS(docTokens, queryTokens, edits)
 
 	// Calculate LCS score
 	lcsScore := float64(len(lcs)) / float64(len(queryTokens))
-	return lcsScore
+
+	// Calculate LCS compactness
+	compactScore := calcCompactScore(lcsIndexes)
+
+	// Return the final score
+	// TODO: Since we are using Myers Diff Algorithm, the compactness score
+	// might be irrelevant, so later we need to check whether to use the
+	// ordinary LCS algorithm (from Wikipedia) or just scrap the compactness
+	// score altogether.
+	return lcsScore*0.6 + compactScore*0.4
+}
+
+func calcCompactScore(indexes []int) float64 {
+	var sum float64
+	for i := 0; i < len(indexes)-1; i++ {
+		nextIdx := float64(indexes[i+1])
+		currentIdx := float64(indexes[i])
+		idxDiff := math.Abs(nextIdx - currentIdx)
+		sum += 1.0 / idxDiff
+	}
+
+	return sum / float64(len(indexes)-1)
 }
